57e08d7e1ecfcc8c59ba5241aca3bc2258cc6fa7
Maria <maria.lmm.975@gmail.com>
2022-05-07 15:31:05 -0300

    Fix createRoot

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,16 +1,24 @@
 import axios from "axios";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 function SignUp(){
+    const {user, setUser} = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
     return(
         <Main>
             <h1>MyWallet</h1>
             <forms>
-                <input type="text" placeholder="Nome" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Senha" />
-                <input type="password" placeholder="Confirme a senha" />
+                <input type="text" placeholder="Nome" value={user.name} />
+                <input type="email" placeholder="Email" value={user.email} />
+                <input type="password" placeholder="Senha" value={user.password}  />
+                <input type="password" placeholder="Confirme a senha" value={user.confirmPassword} />
                 <button type="submit">Cadastrar</button>
             </forms>
             <Link to="/Login" />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import ReactDom from "react-dom";
 
 import App from "./Components/App";
 
-const root = document.querySelector(".root");
+const container = document.querySelector(".root");
 const app = App();
+const root = ReactDom.createRoot(container);
 
-ReactDom.render(app, root);
+root.render(app);
