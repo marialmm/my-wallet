9965395981618ee3805950d8a4643b82cfe0136d
Maria <maria.lmm.975@gmail.com>
2022-05-07 15:36:54 -0300

    Add Global Style

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,17 +4,20 @@ import Home from "./Home";
 import Login from "./Login";
 import New from "./New";
 import SignUp from "./SignUp";
-
+import GlobalStyle from "./../assets/globalStyle/globalStyle";
 
 function App(){
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={Login} />
-            <Route path="/sign-up" element={SignUp} />
-            <Route path="/home" element={Home} />
-            <Route path="/new/:type" element={New} />
-        </Routes>
-    </BrowserRouter>
+    <>
+        <GlobalStyle />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={Login} />
+                <Route path="/sign-up" element={SignUp} />
+                <Route path="/home" element={Home} />
+                <Route path="/new/:type" element={New} />
+            </Routes>
+        </BrowserRouter>
+    </>
 };
 
 export default App;
